118889c4ae03c2a094083388c1a1c5529622c0cf
Filip Tomashevski <93556752+FilipTomash@users.noreply.github.com>
2023-07-27 14:15:25 +0200

    Delete Node.js-adv directory

--- Node.js-adv/Class04-Workshop/const/router.const.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { productRouter } from "../routes/product.routes.js";
-import { orderRouter } from "../routes/order.routes.js";
-
-export const globalRouter = Router();
-
-globalRouter.use("/products", productRouter);
-globalRouter.use("/orders", orderRouter);
